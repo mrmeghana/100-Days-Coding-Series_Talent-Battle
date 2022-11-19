72eb84ecdc8b909c574b99b2f0c261af2f1cf010
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-11-20 00:21:45 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class day3
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if(Character.isLetter(ch))
+        {
+           System.out.println((int)ch);
+        }
+        
+ }
+}
