81ccf4bbe9a864838d032ca1c7d0d12ca1597493
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-11-22 23:59:50 +0530

    Update day7.java

--- day7.java
+++ day7.java
@@ -1,3 +1,16 @@
+/*Write a program to find Number of days in a given month of a given year
+
+Description:
+Get the number of month and year as input from the user and check the number of days present in that month.
+
+Input
+Enter month : 2
+Enter year : 2000
+Output
+29*/
+
+
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
