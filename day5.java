e70cf751c21154709cd9ab46fbb27ce6ac561952
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-11-22 10:30:10 +0530

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class day5
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(num%2==0?"Even":"Odd");
+    }
+}
