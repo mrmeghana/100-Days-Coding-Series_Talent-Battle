7fec4be47723b3b279b92073aac5302deca8fb36
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-12-12 00:30:48 +0530

    Add files via upload

---
+++ Day25.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class Day25
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int side=sc.nextInt();
+        float area;
+        area=(float)3.14*side*side;
+        System.out.println(area);
+    }
+}
